b09046e0dbd9cb48cd99a0a8ea499eba691c7e60
eli _ <metall65eli@gmail.com>
2024-08-19 13:48:48 -0400

    chore: agrego mas tests

--- tests/unit/parser.test.ts
+++ tests/unit/parser.test.ts
@@ -1,5 +1,5 @@
 import { IConsultorWebParser } from '../../src/types/consultor.interfaces';
-import { info_estudiante } from '../../src/types/consultor.types';
+import { info_contacto, info_estudiante, info_tiempo_rendimiento } from '../../src/types/consultor.types';
 import { ConsultorWebParser } from './../../src/core/consultor.webparser';
 import * as utils from './utils';
 
@@ -7,14 +7,30 @@ const path_to_resources = "tests/resources";
 let html_data: string;
 let webparser: IConsultorWebParser; 
 describe("Tests del Web Parser",  ()=>{
-    beforeAll(async ()=>{
+    beforeEach(async ()=>{
          html_data = utils.read_File(`${path_to_resources}/consultor_html_data.txt`);
          webparser = new ConsultorWebParser(html_data);
     });
     it("Debe parsear la cabecera del estudiante ", async ()=>{
         await webparser.parse();
         let cabecera: info_estudiante = await webparser.get_info_estudiante();
-        let resultado_esperado = JSON.parse(utils.read_File(`${path_to_resources}/info_cabecera_estudiante.json`));
+        let resultado_esperado = utils.parse_json(`${path_to_resources}/info_cabecera_estudiante.json`);
         expect(cabecera).toEqual(resultado_esperado);
     });
+
+    it("Debe parsear la informacion de contacto", async ()=>{
+        await webparser.parse();
+        let info_contacto: info_contacto = await webparser.get_info_contacto();
+        let resultado_esperado = utils.parse_json(`${path_to_resources}/info_contacto_estudiante.json`);
+        expect(info_contacto).toEqual(resultado_esperado);
+    });
+
+    it("Debe parsear el tiempo y rendimiento del estudiante", async ()=>{
+        await webparser.parse();
+        let info_rendimiento:info_tiempo_rendimiento = await webparser.get_info_tiempo_rendimiento();
+        let resultado_esperado = utils.parse_json(`${path_to_resources}/info_rendimiento_estudiante.json`);
+        expect(info_rendimiento).toEqual(resultado_esperado);
+    });
+
+
 });
--- tests/unit/utils.ts
+++ tests/unit/utils.ts
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs, { read } from 'fs';
 export function read_File(path:string):string {
     try{
         return fs.readFileSync(path, 'utf-8');
@@ -14,4 +14,12 @@ export function write_file(path:string, content:string):void{
     }catch(error){
         console.log(error);
     }
+}
+
+export function parse_json(path:string){
+    try{
+        return JSON.parse(read_File(path));
+    }catch(error){
+        console.log((error as Error).message);
+    }  
 }
